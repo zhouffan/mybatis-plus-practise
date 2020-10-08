6f767ea8a74ed49ecadbc44b218c378c07c3a2b8
zhouffan <zhouffan@qq.com>
2020-10-08 19:00:46 +0800

    注释

--- src/main/java/com/fw/mybatisplus/MybatisPlusApplication.java
+++ src/main/java/com/fw/mybatisplus/MybatisPlusApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MybatisPlusApplication {
 
+    /**
+     * 启动
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(MybatisPlusApplication.class, args);
     }
